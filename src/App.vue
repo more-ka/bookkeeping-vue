d0a0b63f8b779ad15b0c047bb9046c05e3df30ae
more-ka <1640627628@qq.com>
2020-03-06 23:29:28 +0800

    记账页面微调

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-03-05 17:26:53
- * @LastEditTime: 2020-03-06 20:33:14
+ * @LastEditTime: 2020-03-06 23:28:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \bookkeeping-vue\src\App.vue
@@ -9,18 +9,25 @@
 <template>
   <div id="app">
     <div class="page">
-      <section>
-        <h2 class="title">记账本</h2>
-      </section>
       <section class="tags">
-        <span class="tag">衣</span>
+        <span class="tag active">衣</span>
         <span class="tag">食</span>
         <span class="tag">住</span>
         <span class="tag">行</span>
       </section>
+      <section class="editTag">
+        <span>编辑标签</span>
+      </section>
       <section class="remarks">
         <span>备注</span>
         <input type="text" placeholder="添加备注" />
+      </section>
+      <section class="selectBar">
+        <span class="active">支出</span>
+        <span>收入</span>
+      </section>
+      <section class="numInput">
+        <input type="text" focous>
       </section>
       <section class="keyboard">
         <div class="numberLock">
@@ -62,11 +69,19 @@
           <div class="delete">
             <div class="content">删除</div>
           </div>
+          <div class="clear">
+            <div class="content">清空</div>
+          </div>
           <div class="confirm">
             <div class="content">确定</div>
           </div>
         </div>
       </section>
+    </div>
+    <div class="navBar"> 
+      <router-link :to="{ path: '/app' }" class="item">明细</router-link>
+      <router-link :to="{ path: '/app' }" class="item active">记账</router-link>
+      <router-link :to="{ path: '/app' }" class="item">统计</router-link>
     </div>
   </div>
 </template>
